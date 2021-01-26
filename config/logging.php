07213fbe3a61a4377ecd9c3300213f046a405489
Hernie Jabien <herniejabien45@gmail.com>
2021-01-26 09:28:16 +0800

    config/logging.php single.driver:single => single.driver:errorlog for heroku

--- config/logging.php
+++ config/logging.php
@@ -42,7 +42,8 @@ return [
         ],
 
         'single' => [
-            'driver' => 'single',
+            // 'driver' => 'single',
+            'driver' => 'errorlog',
             'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
         ],
